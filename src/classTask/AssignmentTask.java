5d5c9be7a88288cd99b42616ca3e18b471926730
michalvahab <michalvahab@gmail.com>
2023-03-13 20:47:34 +0200

    assignement task

---
+++ src/classTask/AssignmentTask.java
@@ -0,0 +1,31 @@
+package classTask;
+
+public class AssignmentTask {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("a=" + a);
+
+        a += 90;
+        System.out.println("a=" + a);
+
+        a -= 25;
+        System.out.println("a=" + a);
+
+        a /= 3;
+        System.out.println("a=" + a);
+
+        a %= 10;
+        System.out.println("a=" + a);
+
+        a *= (-3);
+        System.out.println("a=" + a);
+
+        a++;
+        System.out.println("a=" + a);
+
+        a--;
+        System.out.println("a=" + a);
+
+
+    }
+}
--- src/classTask/ClockTask.java
+++ src/classTask/ClockTask.java
@@ -7,6 +7,7 @@ public class ClockTask {
         Scanner input = new Scanner(System.in);
         System.out.println("What time is it now? written in four digits");
         int time = input.nextInt();
+        System.out.println("the time is:");
         System.out.println(time / 100 + ":" + time % 100);
 
 
