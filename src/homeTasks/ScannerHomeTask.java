3dab2ec6f3b01e789f322a2c7e1b675642c8ee39
michalvahab <michalvahab@gmail.com>
2023-03-13 17:56:15 +0200

    homework1

--- src/homeTasks/ScannerHomeTask.java
+++ src/homeTasks/ScannerHomeTask.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class ScannerHomeTask {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System .in);
+        Scanner input = new Scanner(System.in);
         System.out.println("what is the first name?");
-        String firstname=input.nextLine();
+        String firstname = input.nextLine();
         System.out.println("what is the last name?");
-        String lastname=input.nextLine();
+        String lastname = input.nextLine();
         System.out.println("what is your city of residence?");
-        String City=input.nextLine();
+        String City = input.nextLine();
         System.out.println("what is your average grade?");
-       double Average =input.nextDouble();
+        double Average = input.nextDouble();
         System.out.println("what is your age?");
-        int Age= input.nextInt();
+        int Age = input.nextInt();
 
         System.out.println("firstname = " + firstname);
         System.out.println("lastname = " + lastname);
