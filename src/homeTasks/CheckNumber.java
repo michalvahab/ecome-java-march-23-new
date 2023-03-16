1bb7e02454f871a35d96c30bbe71fd56253d01ad
michalvahab <michalvahab@gmail.com>
2023-03-16 02:02:25 +0200

    change

--- src/homeTasks/CheckNumber.java
+++ src/homeTasks/CheckNumber.java
@@ -3,11 +3,11 @@ package homeTasks;
 public class CheckNumber {
     public static void main(String[] args) {
         System.out.println("10%2==0" + " -" + (10 % 2 == 0));
-
         System.out.println("25%2==0" + " -" + (25 % 2 == 0));
         System.out.println("55%2==0" + " -" + (55 % 2 == 0));
         System.out.println("22%2==0" + " -" + (22 % 2 == 0));
         System.out.println("0%2==0" + " -" + (0 % 2 == 0));
+
 
 
     }
