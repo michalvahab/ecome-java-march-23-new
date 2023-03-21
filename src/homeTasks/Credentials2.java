5b92a756b11b9dd7917b6d95c6fbb85586f23f61
michalvahab <michalvahab@gmail.com>
2023-03-21 20:35:05 +0200

    change

--- src/homeTasks/Credentials2.java
+++ src/homeTasks/Credentials2.java
@@ -4,28 +4,24 @@ import java.util.Scanner;
 
 public class Credentials2 {
     public static void main(String[] args) {
-String login;
+        String login;
 
         String saveUsername = "fullName";
 
-        String  savePassword = "password";
+        String savePassword = "password";
 
 
         Scanner input = new Scanner(System.in);
         Scanner input2 = new Scanner(System.in);
 
 
-
         System.out.println("write your username");
         String actualUsername = input.nextLine();
         System.out.println("write your password");
-        String actualPassword =input2.nextLine();
-        login= ((actualUsername.equals(saveUsername)) && ( savePassword.equals(actualPassword))) ?("welcome to" +actualUsername ):"No such user exists";
+        String actualPassword = input2.nextLine();
+        login = ((actualUsername.equals(saveUsername)) && (savePassword.equals(actualPassword))) ? ("welcome to" + actualUsername) : "No such user exists";
 
 
-
-
-
-            }
     }
+}
 
